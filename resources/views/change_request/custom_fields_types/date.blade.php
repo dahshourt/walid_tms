5f48797cfcdebf70562719937029ee3909ced4d6
unknown <tadelalrmony@gmail.com>
2025-03-24 03:39:38 +0200

    cronJob

--- resources/views/change_request/custom_fields_types/date.blade.php
+++ resources/views/change_request/custom_fields_types/date.blade.php
@@ -2,7 +2,7 @@
                                                   
  
 <div class="col-md-6 change-request-form-field field_{{$item->CustomField->name}}">
-    
+ 
     @if(isset($cr))
         <label for="user_type">{{ $item->CustomField->label }} </label>
     @else
@@ -12,7 +12,7 @@
         <span style="color: red;">*</span>
     @endif
         @if((isset($item->enable)&&($item->enable==1)))
-                <input  type="date" name="{{ $item->CustomField->name }}"  class="form-control form-control-lg "  value="{{ old($item->CustomField->name, $custom_field_value) }}"  style="color:black; font-weight: bold;font-size:15px;text-align:center"  />         
+                <input  type="date" name="{{ $item->CustomField->name }}"  class="form-control form-control-lg "  value="{{ old($item->CustomField->name, $cr->calendar) }}"  style="color:black; font-weight: bold;font-size:15px;text-align:center"  />         
         @else
             @if(isset($cr))
                 <label class="form-control form-control-lg">{{ ($cr->{$item->CustomField->name}) }} </label>
