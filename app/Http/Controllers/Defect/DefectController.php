c09887cb82e378542d6e1fd9a84d5824b202a95e
unknown <tadelalrmony@gmail.com>
2025-03-25 11:28:51 +0200

    technicalTeamIssue

--- app/Http/Controllers/Defect/DefectController.php
+++ app/Http/Controllers/Defect/DefectController.php
@@ -16,6 +16,7 @@ use App\Factories\Defect\DefectFactory;
 use App\Http\Repository\Defect\DefectRepository;
 use App\Models\Technical_team;
 use App\Models\Status;
+use App\Models\Group;
 use App\Models\DefectAttachment;
 use App\Factories\Statuses\StatusFactory;
 
@@ -71,7 +72,7 @@ class DefectController extends Controller
         {
             return redirect()->back()->with('status' , 'You have no access to edit this CR' );
         } // to check if the user has access to edit this cr or not 
-         $technical_team = Technical_team::all();
+          $technical_team =  Group::where('technical_team', 1)->get();
          $defect_status = $this->status->get_defect_status();
          $CustomFields = $this->custom_field_group_type->getAllCustomFieldsWithSelectedByformType("form_type", 7);
 
@@ -132,7 +133,8 @@ class DefectController extends Controller
     public function edit($id)
     {
         //custom fields
-        $technical_team = Technical_team::all();
+        //$technical_team = Technical_team::all();
+        $technical_team =  Group::where('technical_team', 1)->get();
         $defect_status = $this->status->get_defect_status();
         $CustomFields = $this->custom_field_group_type->getAllCustomFieldsWithSelectedByformType("form_type", 7);
         //get Defect data
