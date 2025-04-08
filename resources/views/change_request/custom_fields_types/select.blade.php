b07366ff5cadd251b308a8e67e634c3a023d0e61
Mahmoud Abdelrahman Madbolie Bastawisy <Mahmoud.bastawisy@te.eg>
2025-04-08 22:01:12 +0200

    refactor change request

--- resources/views/change_request/custom_fields_types/select.blade.php
+++ resources/views/change_request/custom_fields_types/select.blade.php
@@ -1,173 +1,147 @@
-@if($item->CustomField->type == "select")
-    <div class="col-md-6 change-request-form-field field_{{$item->CustomField->name}}">
-        <label for="user_type">{{ $item->CustomField->label }} </label>
-        @if(isset($item->validation_type_id) && ($item->validation_type_id == 1))
-            <span style="color: red;">*</span>
-        @endif
+@if($item->CustomField->type === "select")
+    @php
+        $fieldName = $item->CustomField->name;
+        $fieldLabel = $item->CustomField->label;
+        $isRequired = isset($item->validation_type_id) && $item->validation_type_id == 1;
+        $isEnabled = isset($item->enable) && $item->enable == 1;
+        $hasPermissionRestriction = in_array($fieldName, ['tester_id', 'designer_id', 'developer_id']);
+    @endphp
 
-        @if(!isset($cr) && $item->CustomField->name === 'application_id')
-            <select name="{{ $item->CustomField->name }}" class="form-control form-control-lg">
-                <option value="{{$target_system->id}}">{{$target_system->name}}</option>
-            </select>
-        @elseif($item->CustomField->name == "cr_member")
-            <select name="{{ $item->CustomField->name }}" class="form-control form-control-lg">
-                <option value="">Select</option>
-                @foreach($item->CustomField->getCustomFieldValue() as $value)
-                    @if($value->defualt_group->title === 'CR Team Admin')
-                        <option value="{{ $value->id }}" {{ $custom_field_value == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
-                    @endif
-                @endforeach
-            </select>
-        @else
-            <select name="{{ $item->CustomField->name }}" id="{{ $item->CustomField->name }}" class="form-control form-control-lg" 
-                @if(isset($item->validation_type_id) && $item->validation_type_id == 1) required @endif
-                @cannot('Set Time For Another User')
-                    @if($item->CustomField->name == 'tester_id' || $item->CustomField->name == 'designer_id' || $item->CustomField->name == 'developer_id')
-                        disabled
-                    @endif
-                @endcannot
-                {{ (isset($item->enable) && ($item->enable == 1)) ? 'enabled' : 'disabled' }}>
+    <div class="col-md-6 change-request-form-field field_{{ $fieldName }}">
+        <label for="{{ $fieldName }}">{{ $fieldLabel }}</label>
+        @if($isRequired)<span style="color: red;">*</span>@endif
 
-                @cannot('Set Time For Another User')
-                    @if($item->CustomField->name == 'tester_id' || $item->CustomField->name == 'designer_id' || $item->CustomField->name == 'developer_id')
-                        <option value="{{ auth()->user()->id }}" selected>{{ auth()->user()->name }}</option>
-                    @endif
-                @endcannot
+        @switch(true)
+            @case(!isset($cr) && $fieldName === 'application_id')
+                <select name="{{ $fieldName }}" class="form-control form-control-lg">
+                    <option value="{{ $target_system->id }}">{{ $target_system->name }}</option>
+                </select>
+                @break
 
-                @if($item->CustomField->name == "new_status_id")
-                    <option value="{{$cr->getCurrentStatus()?->status?->status_name}}" disabled selected>{{ $cr->getCurrentStatus()?->status?->status_name }}</option>
-                    @foreach($cr->set_status as $status)
-                        @if($status->same_time == 1)
-                            <option value="{{ $status->id }}" {{ $custom_field_value == $status->id ? 'selected' : '' }}>{{ $status->to_status_label }}</option>
-                        @else
-                            <option value="{{ $status->id }}" 
+            @case($fieldName === 'cr_member')
+                <select name="{{ $fieldName }}" class="form-control form-control-lg">
+                    <option value="">Select</option>
+                    @foreach($item->CustomField->getCustomFieldValue() as $value)
+                        @if($value->defualt_group->title === 'CR Team Admin')
+                            <option value="{{ $value->id }}" {{ $custom_field_value == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
+                        @endif
+                    @endforeach
+                </select>
+                @break
+
+            @default
+                <select
+                    name="{{ $fieldName }}"
+                    id="{{ $fieldName }}"
+                    class="form-control form-control-lg"
+                    {{ $isRequired ? 'required' : '' }}
+                    @cannot('Set Time For Another User')
+                        {{ $hasPermissionRestriction ? 'disabled' : '' }}
+                    @endcannot
+                    {{ $isEnabled ? 'enabled' : 'disabled' }}
+                >
+                    @cannot('Set Time For Another User')
+                        @if($hasPermissionRestriction)
+                            <option value="{{ auth()->user()->id }}" selected>{{ auth()->user()->name }}</option>
+                        @endif
+                    @endcannot
+
+                    @if($fieldName === 'new_status_id')
+                        <option value="{{ $cr->getCurrentStatus()?->status?->status_name }}" disabled selected>{{ $cr->getCurrentStatus()?->status?->status_name }}</option>
+                        @foreach($cr->set_status as $status)
+                            <option value="{{ $status->id }}"
                                 {{ $custom_field_value == $status->id ? 'selected' : '' }}
-                                data-status-name="{{ $status->workflowstatus[0]->to_status->status_name }}"
-                                data-defect="{{ $status->workflowstatus[0]->to_status->defect }}">
-                                @if($status->workflowstatus[0]->to_status->high_level)
-                                {{$status->workflowstatus[0]->to_status->high_level->name}}
-                                @else
-                                    @if($status->to_status_label)
-                                        {{$status->to_status_label }}
-                                    @else
-                                        {{$status->workflowstatus[0]->to_status->status_name }}
-                                    @endif
+                                @if(!$status->same_time)
+                                    data-status-name="{{ $status->workflowstatus[0]->to_status->status_name }}"
+                                    data-defect="{{ $status->workflowstatus[0]->to_status->defect }}"
                                 @endif
+                            >
+                                {{ $status->workflowstatus[0]->to_status->high_level->name ?? $status->to_status_label ?? $status->workflowstatus[0]->to_status->status_name }}
                             </option>
-                        @endif
-                    @endforeach
-                @elseif($item->CustomField->name == "release_name")
-                    <option value=""> select </option>
-                    @foreach($cr->get_releases() as $release)
-                        <option value="{{ $release->id }}" {{ $custom_field_value == $release->id ? 'selected' : '' }}>{{ $release->name }} </option>
-                    @endforeach
-                @else
-                    @if((isset($item->enable) && ($item->enable == 1)))
-                        <option value="">Select</option>
-                        @if($item->CustomField->name == "developer_id")
-                            @foreach($developer_users as $developer)
-                                <option value="{{ $developer->id }}" {{ old($developer->user_name, $custom_field_value) == $developer->id ? 'selected' : '' }}>{{ $developer->user_name }}</option>
-                            @endforeach
-                        @endif
-                        @if($item->CustomField->name == "tester_id")
-                            @foreach($testing_users as $users)
-                                <option value="{{ $users->id }}" {{ old($users->user_name, $custom_field_value) == $users->id ? 'selected' : '' }}>{{ $users->user_name }}</option>
-                            @endforeach
-                        @endif
-                        @if($item->CustomField->name == "designer_id")
-                            @foreach($sa_users as $users)
-                                <option value="{{ $users->id }}" {{ old($users->user_name, $custom_field_value) == $users->id ? 'selected' : '' }}>{{ $users->user_name }}</option>
-                            @endforeach
-                        @endif
-
-                        @foreach($item->CustomField->getCustomFieldValue() as $value)
-                            @if($item->CustomField->name == "developer_id")
-                            @elseif($item->CustomField->name == "tester_id")
-                            @elseif($item->CustomField->name == "designer_id")
-                            @else
-                                @if(isset($cr))
-                                    <option value="{{ $value->id }}" {{ old($item->CustomField->name, $custom_field_value) == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
-                                @else
-                                    <option value="{{ $value->id }}">{{ $value->name }}</option>
-                                @endif
-                            @endif
                         @endforeach
-                    @else
-                        @php
-                            $selectedValue = "";
-                            if(isset($cr)) $selectedValue = old($item->CustomField->name, $custom_field_value);
-                        @endphp
 
-                        @if($selectedValue)
+                    @elseif($fieldName === 'release_name')
+                        <option value="">Select</option>
+                        @foreach($cr->get_releases() as $release)
+                            <option value="{{ $release->id }}" {{ $custom_field_value == $release->id ? 'selected' : '' }}>{{ $release->name }}</option>
+                        @endforeach
+
+                    @else
+                        @if($isEnabled)
+                            <option value="">Select</option>
+
+                            @php
+                                $userList = [
+                                    'developer_id' => $developer_users ?? [],
+                                    'tester_id' => $testing_users ?? [],
+                                    'designer_id' => $sa_users ?? []
+                                ];
+                            @endphp
+
+                            @if(isset($userList[$fieldName]))
+                                @foreach($userList[$fieldName] as $user)
+                                    <option value="{{ $user->id }}" {{ old($user->user_name, $custom_field_value) == $user->id ? 'selected' : '' }}>{{ $user->user_name }}</option>
+                                @endforeach
+                            @endif
+
                             @foreach($item->CustomField->getCustomFieldValue() as $value)
-                                @if($value->id == $selectedValue)
-                                    <option value="{{ $value->id }}" selected>{{ $value->name }}</option>
-                                @endif
+                                @unless(in_array($fieldName, array_keys($userList)))
+                                    <option value="{{ $value->id }}" {{ (isset($cr) && old($fieldName, $custom_field_value) == $value->id) ? 'selected' : '' }}>{{ $value->name }}</option>
+                                @endunless
                             @endforeach
                         @else
-                            <option value="">Select</option>
+                            @php
+                                $selectedValue = isset($cr) ? old($fieldName, $custom_field_value) : '';
+                            @endphp
+
+                            @if($selectedValue)
+                                @foreach($item->CustomField->getCustomFieldValue() as $value)
+                                    @if($value->id == $selectedValue)
+                                        <option value="{{ $value->id }}" selected>{{ $value->name }}</option>
+                                    @endif
+                                @endforeach
+                            @else
+                                <option value="">Select</option>
+                            @endif
                         @endif
                     @endif
-                @endif
-            </select>
-        @endif
+                </select>
+        @endswitch
     </div>
 @endif
-<?php 
-    $def1 = 0;
-    $def2 = 0;
-    if(isset($cr))
-    {
-        $def1= $cr->defects()->count(); 
-        $def2=  $cr->defects()->whereIn('status_id', [86, 87])->count();
-    }
 
-?>
-@if($def1 != $def2)
+@php
+    $def1 = isset($cr) ? $cr->defects()->count() : 0;
+    $def2 = isset($cr) ? $cr->defects()->whereIn('status_id', [86, 87])->count() : 0;
+@endphp
 
+@if($def1 !== $def2)
     <script>
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    async function checkStatusBeforeSubmit(event) {
-        let selectElement = document.querySelector('select[name="new_status_id"]');
-        if (!selectElement) {
-            alert("Dropdown not found");
-            return;
-        }
+        document.addEventListener("DOMContentLoaded", function () {
+            document.querySelector("form")?.addEventListener("submit", async function checkStatusBeforeSubmit(event) {
+                const selectElement = document.querySelector('select[name="new_status_id"]');
+                const selectedOption = selectElement?.options[selectElement.selectedIndex];
+                const defectValue = selectedOption?.getAttribute('data-defect') || "0";
 
-        let selectedOption = selectElement.options[selectElement.selectedIndex];
-        let defectValue = selectedOption?.getAttribute('data-defect') || "0";
-     
+                if (defectValue === "1") {
+                    event.preventDefault();
 
-        console.log("Defect Value:", defectValue);
+                    const result = await Swal.fire({
+                        title: 'Are you sure?',
+                        text: "There are defects related to this CRS. Are you sure you want to continue?",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, continue!',
+                        cancelButtonText: 'No, cancel!'
+                    });
 
-       
-
-        if (defectValue == "1") {
-          
-            event.preventDefault();
-
-            const result = await Swal.fire({
-                title: 'Are you sure?',
-                text: "There are defects related to this CRS. Are you sure you want to continue?",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, continue!',
-                cancelButtonText: 'No, cancel!'
-            });
-
-            if (result.isConfirmed) {
-                document.querySelector("form").submit();
-            }
-        }
-
-        
-    }
-
-    document.querySelector("form")?.addEventListener("submit", checkStatusBeforeSubmit, { once: true });
-});
-
+                    if (result.isConfirmed) {
+                        document.querySelector("form").submit();
+                    }
+                }
+            }, { once: true });
+        });
     </script>
 @endif
