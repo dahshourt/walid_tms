6181f56b600b64895a62c050f0b22984e9b572d3
Walid Dahshour Salama <Walid.Dahshour@te.eg>
2025-10-14 00:30:53 +0300

    add validation numeric and required

--- resources/views/change_request/custom_fields_types/input.blade.php
+++ resources/views/change_request/custom_fields_types/input.blade.php
@@ -1,13 +1,25 @@
-@if($item->CustomField->type == "input")
+@if ($item->CustomField->type == "input")
     @php
-        $fieldName = $item->CustomField->name;
+        $fieldName  = $item->CustomField->name;
         $fieldLabel = $item->CustomField->label;
-        $isRequired = isset($item->validation_type_id) && $item->validation_type_id == 1;
-        $isEditable = isset($item->enable) && $item->enable == 1;
-        $isEstimationField = in_array($fieldName, ['dev_estimation','design_estimation','testing_estimation']);
-        $fieldType = $isEstimationField ? 'number' : ($fieldName === 'requester_email' || $fieldName === 'division_manager' ? 'email' : 'text');
 
-        // Determine value
+        $validationType = $item->validation_type_id ?? null;
+        $isRequired = in_array($validationType, [1, 3]); // 1 = Required, 3 = Numeric Required
+        $isNumeric  = in_array($validationType, [2, 3]); // 2 or 3 => numeric type
+
+        $isEditable = isset($item->enable) && $item->enable == 1;
+        $isEstimationField = in_array($fieldName, ['dev_estimation', 'design_estimation', 'testing_estimation']);
+
+        // Determine final HTML input type
+        if ($isNumeric || $isEstimationField) {
+            $fieldType = 'number';
+        } elseif (in_array($fieldName, ['requester_email', 'division_manager'])) {
+            $fieldType = 'email';
+        } else {
+            $fieldType = 'text';
+        }
+
+        // Determine input value
         if (isset($cr)) {
             $fieldValue = old($fieldName, $cr->{$fieldName} ?? ($custom_field_value ?? ''));
         } else {
@@ -17,59 +29,58 @@
 
     <div class="col-md-6 change-request-form-field field_{{ $fieldName }}">
         <label for="{{ $fieldName }}">{{ $fieldLabel }}</label>
-        @if($isRequired)
-            <span style="color: red;">*</span>
+        @if ($isRequired)
+            <span class="text-danger">*</span>
         @endif
 
-     
-        @if($isEditable || $isEstimationField)
-
-            {{-- Special case: division_manager --}}
-            @if($fieldName === 'division_manager')
-                <input 
-                    type="email" 
+        @if ($isEditable)
+            {{-- Special Case: Division Manager --}}
+            @if ($fieldName === 'division_manager')
+                <input
+                    type="email"
                     id="division_manager"
-                    name="{{ $fieldName }}" 
-                    class="form-control form-control-lg @error($fieldName) is-invalid @enderror" 
+                    name="{{ $fieldName }}"
+                    class="form-control form-control-lg @error($fieldName) is-invalid @enderror"
                     value="{{ $fieldValue }}"
-                    @if($isRequired) required @endif
+                    @if ($isRequired) required @endif
                 />
                 @error($fieldName)
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
                 <small id="email_feedback" class="form-text text-danger"></small>
 
-            
+            {{-- Requester name/email special cases --}}
             @elseif(in_array($fieldName, ['requester_name', 'requester_email']))
-                <input 
+                <input
                     type="{{ $fieldType }}"
                     name="{{ $fieldName }}"
                     class="form-control form-control-lg @error($fieldName) is-invalid @enderror"
                     value="{{ $fieldName === 'requester_name' ? auth()->user()->name : auth()->user()->email }}"
                     readonly
-                    @if($isRequired) required @endif
+                    @if ($isRequired) required @endif
                 />
                 @error($fieldName)
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
 
-         
+            {{-- Normal editable input --}}
             @else
-                <input 
+                <input
                     type="{{ $fieldType }}"
                     name="{{ $fieldName }}"
                     class="form-control form-control-lg @error($fieldName) is-invalid @enderror"
                     value="{{ $fieldValue }}"
-                    @if($isRequired) required @endif
+                    @if ($isRequired) required @endif
+                    @if ($isNumeric) step="any" @endif
                 />
                 @error($fieldName)
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
             @endif
 
-       
+        {{-- Read-only (disabled) view --}}
         @elseif(isset($cr))
-            <input 
+            <input
                 type="{{ $fieldType }}"
                 name="{{ $fieldName }}"
                 class="form-control form-control-lg"
