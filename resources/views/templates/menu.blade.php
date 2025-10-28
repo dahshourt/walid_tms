a53a1a07597031f1c1ee5684dd855c20c575532a
Ahmed Omar Omar Hassan <Ahmed.O.Hasan@te.eg>
2025-10-28 10:13:41 +0300

    Update and Merge Menu

--- resources/views/templates/menu.blade.php
+++ resources/views/templates/menu.blade.php
@@ -554,7 +554,7 @@
 												</div>
 											</li>
 											@endcan
-                                            @canany(['Final Confirmation', 'List Custom Fields'])
+                                            @canany(['Final Confirmation', 'Edit Testable Form', 'Admin Add Attachments and Feedback', 'List Custom Fields'])
                                                 <li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
                                                     <a href="javascript:;" class="menu-link menu-toggle">
                                                         <span class="menu-text">Admin</span>
@@ -563,6 +563,45 @@
                                                     </a>
                                                     <div class="menu-submenu menu-submenu-classic menu-submenu-left">
                                                         <ul class="menu-subnav">
+															@can('Edit Testable Form')
+																	<li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
+																		<a href="{{ url('testable_form') }}" class="menu-link">
+																		<span class="svg-icon menu-icon">
+																							<!--begin::Svg Icon | path:assets/media/svg/icons/General/Shield-check.svg-->
+																							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+																								<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																									<rect x="0" y="0" width="24" height="24" />
+																									<path d="M4,4 L11.6314229,2.5691082 C11.8750185,2.52343403 12.1249815,2.52343403 12.3685771,2.5691082 L20,4 L20,13.2830094 C20,16.2173861 18.4883464,18.9447835 16,20.5 L12.5299989,22.6687507 C12.2057287,22.8714196 11.7942713,22.8714196 11.4700011,22.6687507 L8,20.5 C5.51165358,18.9447835 4,16.2173861 4,13.2830094 L4,4 Z" fill="#000000" opacity="0.3" />
+																									<path d="M11.1750002,14.75 C10.9354169,14.75 10.6958335,14.6541667 10.5041669,14.4625 L8.58750019,12.5458333 C8.20416686,12.1625 8.20416686,11.5875 8.58750019,11.2041667 C8.97083352,10.8208333 9.59375019,10.8208333 9.92916686,11.2041667 L11.1750002,12.45 L14.3375002,9.2875 C14.7208335,8.90416667 15.2958335,8.90416667 15.6791669,9.2875 C16.0625002,9.67083333 16.0625002,10.2458333 15.6791669,10.6291667 L11.8458335,14.4625 C11.6541669,14.6541667 11.4145835,14.75 11.1750002,14.75 Z" fill="#000000" />
+																								</g>
+																							</svg>
+																							<!--end::Svg Icon-->
+																						</span>
+																		<span class="menu-text">Testable Form</span>
+
+																		</a>
+																	</li>
+															@endcan
+
+															@can('Admin Add Attachments and Feedback')
+																	<li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
+																		<a href="{{ url('add_attachments_form') }}" class="menu-link">
+																		<span class="svg-icon menu-icon">
+																							<!--begin::Svg Icon | path:assets/media/svg/icons/General/Shield-check.svg-->
+																							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+																								<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																									<rect x="0" y="0" width="24" height="24" />
+																									<path d="M4,4 L11.6314229,2.5691082 C11.8750185,2.52343403 12.1249815,2.52343403 12.3685771,2.5691082 L20,4 L20,13.2830094 C20,16.2173861 18.4883464,18.9447835 16,20.5 L12.5299989,22.6687507 C12.2057287,22.8714196 11.7942713,22.8714196 11.4700011,22.6687507 L8,20.5 C5.51165358,18.9447835 4,16.2173861 4,13.2830094 L4,4 Z" fill="#000000" opacity="0.3" />
+																									<path d="M11.1750002,14.75 C10.9354169,14.75 10.6958335,14.6541667 10.5041669,14.4625 L8.58750019,12.5458333 C8.20416686,12.1625 8.20416686,11.5875 8.58750019,11.2041667 C8.97083352,10.8208333 9.59375019,10.8208333 9.92916686,11.2041667 L11.1750002,12.45 L14.3375002,9.2875 C14.7208335,8.90416667 15.2958335,8.90416667 15.6791669,9.2875 C16.0625002,9.67083333 16.0625002,10.2458333 15.6791669,10.6291667 L11.8458335,14.4625 C11.6541669,14.6541667 11.4145835,14.75 11.1750002,14.75 Z" fill="#000000" />
+																								</g>
+																							</svg>
+																							<!--end::Svg Icon-->
+																						</span>
+																		<span class="menu-text">Add Attachments and Feedback</span>
+
+																		</a>
+																	</li>
+															@endcan
                                                             @can('Final Confirmation')
                                                                 <li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
                                                                     <a href="{{ route('final_confirmation.index') }}" class="menu-link">
@@ -754,45 +793,6 @@
 																							<!--end::Svg Icon-->
 																						</span>
 																			<span class="menu-text">Defects</span>
-
-																		</a>
-																	</li>
-																	@endcan
-																	@can('Edit Testable Form')
-																	<li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
-																		<a href="{{ url('testable_form') }}" class="menu-link">
-																		<span class="svg-icon menu-icon">
-																							<!--begin::Svg Icon | path:assets/media/svg/icons/General/Shield-check.svg-->
-																							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																								<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																									<rect x="0" y="0" width="24" height="24" />
-																									<path d="M4,4 L11.6314229,2.5691082 C11.8750185,2.52343403 12.1249815,2.52343403 12.3685771,2.5691082 L20,4 L20,13.2830094 C20,16.2173861 18.4883464,18.9447835 16,20.5 L12.5299989,22.6687507 C12.2057287,22.8714196 11.7942713,22.8714196 11.4700011,22.6687507 L8,20.5 C5.51165358,18.9447835 4,16.2173861 4,13.2830094 L4,4 Z" fill="#000000" opacity="0.3" />
-																									<path d="M11.1750002,14.75 C10.9354169,14.75 10.6958335,14.6541667 10.5041669,14.4625 L8.58750019,12.5458333 C8.20416686,12.1625 8.20416686,11.5875 8.58750019,11.2041667 C8.97083352,10.8208333 9.59375019,10.8208333 9.92916686,11.2041667 L11.1750002,12.45 L14.3375002,9.2875 C14.7208335,8.90416667 15.2958335,8.90416667 15.6791669,9.2875 C16.0625002,9.67083333 16.0625002,10.2458333 15.6791669,10.6291667 L11.8458335,14.4625 C11.6541669,14.6541667 11.4145835,14.75 11.1750002,14.75 Z" fill="#000000" />
-																								</g>
-																							</svg>
-																							<!--end::Svg Icon-->
-																						</span>
-																		<span class="menu-text">Testable Form</span>
-
-																		</a>
-																	</li>
-																	@endcan
-
-																	@can('Admin Add Attachments and Feedback')
-																	<li class="menu-item menu-item-submenu menu-item-rel" data-menu-toggle="click" aria-haspopup="true">
-																		<a href="{{ url('add_attachments_form') }}" class="menu-link">
-																		<span class="svg-icon menu-icon">
-																							<!--begin::Svg Icon | path:assets/media/svg/icons/General/Shield-check.svg-->
-																							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																								<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																									<rect x="0" y="0" width="24" height="24" />
-																									<path d="M4,4 L11.6314229,2.5691082 C11.8750185,2.52343403 12.1249815,2.52343403 12.3685771,2.5691082 L20,4 L20,13.2830094 C20,16.2173861 18.4883464,18.9447835 16,20.5 L12.5299989,22.6687507 C12.2057287,22.8714196 11.7942713,22.8714196 11.4700011,22.6687507 L8,20.5 C5.51165358,18.9447835 4,16.2173861 4,13.2830094 L4,4 Z" fill="#000000" opacity="0.3" />
-																									<path d="M11.1750002,14.75 C10.9354169,14.75 10.6958335,14.6541667 10.5041669,14.4625 L8.58750019,12.5458333 C8.20416686,12.1625 8.20416686,11.5875 8.58750019,11.2041667 C8.97083352,10.8208333 9.59375019,10.8208333 9.92916686,11.2041667 L11.1750002,12.45 L14.3375002,9.2875 C14.7208335,8.90416667 15.2958335,8.90416667 15.6791669,9.2875 C16.0625002,9.67083333 16.0625002,10.2458333 15.6791669,10.6291667 L11.8458335,14.4625 C11.6541669,14.6541667 11.4145835,14.75 11.1750002,14.75 Z" fill="#000000" />
-																								</g>
-																							</svg>
-																							<!--end::Svg Icon-->
-																						</span>
-																		<span class="menu-text">Add Attachments and Feedback</span>
 
 																		</a>
 																	</li>
