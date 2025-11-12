7ad8fff439117d13ed34fecb0e57305869232391
Ahmed M. Sadik <ahmed.sadik.dev@gmail.com>
2025-11-12 12:36:27 +0200

    New 'Original Statuses' column

--- app/Models/HighLevelStatuses.php
+++ app/Models/HighLevelStatuses.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class HighLevelStatuses extends Model
 {
@@ -18,6 +19,11 @@ class HighLevelStatuses extends Model
     ];
 
     public function status()
+    {
+        return $this->hasMany(Status::class, 'high_level_status_id');
+    }
+
+    public function statuses(): HasMany
     {
         return $this->hasMany(Status::class, 'high_level_status_id');
     }
