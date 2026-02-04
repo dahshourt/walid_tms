4f50d17b0e3796a631969ffd77c59982de138d4b
Ahmed Omar Omar Hassan <Ahmed.O.Hasan@te.eg>
2026-02-04 13:13:29 +0200

    Fix Defect Created

--- app/Events/DefectCreated.php
+++ app/Events/DefectCreated.php
@@ -7,6 +7,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Change_request;
 
 class DefectCreated
 {
